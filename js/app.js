db7f80b79ff408e503dbbeb7b4846b7ae94eb161
erina jahan <erinajahan3216@gmail.com>
2022-03-12 16:17:32 +0600

    question

--- js/app.js
+++ js/app.js
@@ -13,14 +13,14 @@ const getReportedPosts = () => {
 
 const isLiked = (id) => {
   return likedPostsId?.length && !!likedPostsId.includes(id);
-  console.log(isLiked);
-
 };
 
 const addToLiked = (id) => {
-  likedPostsId.push(id);
-  showPosts(posts);
-
+  if (likedPostsId.indexOf(id) === -1) {
+    likedPostsId.push(id);
+  }
+  const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
+  showPosts(remainingPosts);
 };
 
 const reportPost = (id) => {
@@ -30,10 +30,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-
-  return text.length > 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-  // console.log(text);
-
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -41,28 +38,29 @@ const switchTab = (id) => {
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("question").style.display = "block";
+
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("question").style.display = "none";
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
-
+    document.getElementById("question").style.display = "none";
     displayReportedPosts();
   }
 };
 
 const createPost = (post) => {
-  console.log(post);
   const image = post.image;
+  const userImage = post.userImage;
   const div = document.createElement("article");
-
   div.classList.add("post");
-
   div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -71,7 +69,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${post.userImage}" alt="User Picture" />
+                    <img src="${userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -104,19 +102,17 @@ const createPost = (post) => {
 
                   <div class="post__indicators"></div>
 
-                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id
-    })">
+                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id})">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
 
                 <div class="post__content">${displayContent(post.description)}</div>
-                
 
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
+                      <img src="${userImage}" alt="User Picture" />
                     </a>
 
                     <span>Liked by
@@ -129,10 +125,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
-                          
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -153,19 +148,20 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").innerHTML = '';
   const likedPosts = getLikedPosts();
-  console.log(posts);
-  posts.forEach((post) => {
 
+  likedPosts.forEach((post) => {
     const div = createPost(post);
-
     document.getElementById("liked").appendChild(div);
   });
 };
 
 const displayReportedPosts = () => {
+  document.getElementById("reported").innerHTML = '';
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
@@ -176,5 +172,23 @@ const loadPosts = async () => {
   posts = await data.json();
   showPosts(posts);
 }
+
+// select all accordion items
+const accItems = document.querySelectorAll(".accordion__item");
+
+// add a click event for all items
+accItems.forEach((acc) => acc.addEventListener("click", toggleAcc));
+
+function toggleAcc() {
+  // remove active class from all items exept the current item (this)
+  accItems.forEach((item) => item != this ? item.classList.remove("accordion__item--active") : null
+  );
+
+  // toggle active class on current item
+  if (this.classList != "accordion__item--active") {
+    this.classList.toggle("accordion__item--active");
+  }
+}
+
 
 loadPosts();
